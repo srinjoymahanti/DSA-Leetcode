951fc18339a6cb304956d7dfa0dcb74fb52e0a8c
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-02-20 00:00:12 +0530

    Time: 2232 ms (5.01%) | Memory: 74.6 MB (5.06%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,7 +1,49 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        int ele=nums[nums.length-k];
-        return ele;
+    static int ans;
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public int partition(int[] arr, int lo, int hi){
+        int mid = (lo+hi)/2;
+        int pivot = arr[mid], pivotIdx = mid;
+        int smallerCount = 0;
+        for(int i=lo;i<=hi;i++){
+            if(i==mid) continue;
+            if(arr[i]<=pivot) smallerCount++;
+        }
+        int correctIdx = lo + smallerCount;
+        swap(arr,pivotIdx,correctIdx);
+        // partition
+        int i = lo, j = hi;
+        while(i<correctIdx && j>correctIdx){
+            if(arr[i]<=pivot) i++;
+            else if(arr[j]>pivot) j--;
+            else if(arr[i]>pivot && arr[j]<=pivot){
+                swap(arr,i,j);
+            }
+        }
+        return correctIdx;
+    }
+    public void quickSelect(int[] arr, int lo, int hi, int k){
+        if(lo>hi) return;
+        if(lo==hi){
+            if(lo==k-1) ans = arr[lo];
+            return;
+        }
+        int idx = partition(arr,lo,hi);
+        if(idx==k-1){
+            ans = arr[idx];
+            return;
+        }
+        if(k-1 < idx) quickSelect(arr,lo,idx-1,k);
+        else quickSelect(arr,idx+1,hi,k);
+    }
+    public int findKthLargest(int[] arr, int k) {
+        ans = -1;
+        int n = arr.length;
+        quickSelect(arr,0,n-1,n-k+1);
+        return ans;
     }
 }
