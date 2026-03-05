d3494d7e1d161dab34921486de3d65c3ceb92c8c
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-03-05 11:57:14 +0530

    Time: 40 ms (83.11%) | Memory: 55.8 MB (10.24%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -4,7 +4,7 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function(nums, fn, init) {
+function reduce(nums, fn, init) {
     let val=init;
     for(i=0;i<nums.length;i++){
         val=fn(val,nums[i]);
