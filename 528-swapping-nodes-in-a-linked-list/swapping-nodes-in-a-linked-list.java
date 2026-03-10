50e526bc78a46c7ae1f0c72ec71a29e29143335f
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-03-10 17:13:04 +0530

    Time: 1 ms (99.98%) | Memory: 119.2 MB (42.46%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -10,25 +10,42 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-       int size=0;
-       ListNode temp=head;
-       while(temp!=null){
-        size++;
-        temp=temp.next;
-       }
-       int firstIdx=k;
-       int lastIdx=size-k+1;
-       ListNode fast=head;
-       ListNode slow=head;
-       for(int i=1;i<lastIdx;i++){
+        ListNode temp=head;
+        ListNode slow=head;
+        ListNode fast=head;
+        for(int i=1;i<k;i++){
+            temp=temp.next;
+            fast=fast.next;
+        }
         fast=fast.next;
-       }
-       for(int i=1;i<firstIdx;i++){
-        slow=slow.next;
-       }
-       int t=fast.val;
-       fast.val=slow.val;
+        while(fast!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        int t=temp.val;
+       temp.val=slow.val;
        slow.val=t;
        return head;
+
+    //    int size=0;
+    //    ListNode temp=head;
+    //    while(temp!=null){
+    //     size++;
+    //     temp=temp.next;
+    //    }
+    //    int firstIdx=k;
+    //    int lastIdx=size-k+1;
+    //    ListNode fast=head;
+    //    ListNode slow=head;
+    //    for(int i=1;i<lastIdx;i++){
+    //     fast=fast.next;
+    //    }
+    //    for(int i=1;i<firstIdx;i++){
+    //     slow=slow.next;
+    //    }
+    //    int t=fast.val;
+    //    fast.val=slow.val;
+    //    slow.val=t;
+    //    return head;
     }
 }
