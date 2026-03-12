c06c78d0ed44404c01086393281290b2647601ee
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-03-12 15:00:25 +0530

    Time: 67 ms (6.53%) | Memory: 55.3 MB (37.70%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -5,11 +5,11 @@
 var compose = function(functions) {
     
     return function(x) {
-        let result=x;
         for(let i=functions.length-1;i>=0;i--){
-            result=functions[i](result);
+            const fn=functions[i];
+            x=fn(x);
         }
-        return result;
+        return x;
     }
 };
 
