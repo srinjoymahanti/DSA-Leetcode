543c42d3082a6772f2b375850772c3f2b0119abf
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-04-09 11:17:45 +0530

    Time: 2 ms (91.13%) | Memory: 47.4 MB (94.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -15,14 +15,27 @@ class Solution {
         //     }
         // }
 
+        // int j=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]!=0){
+        //         int temp=arr[i];
+        //         arr[i]=arr[j];
+        //         arr[j]=temp;
+        //         j++;
+        //     }
+        // }
+
+        int n=arr.length;
+        int[] arr2=new int[n];
         int j=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-                j++;
-            }
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0) arr2[j++]=arr[i];
+        }
+        while(j<n){
+            arr2[j++]=0;
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=arr2[i];
         }
     }
 }
