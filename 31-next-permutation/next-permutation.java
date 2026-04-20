8b7ba02b18976e393a3eea7cf136c3122d5b4079
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-04-20 09:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (96.49%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -14,18 +14,18 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length;
         int i=n-2;
-        while(i>=0 && nums[i]>=nums[i+1]){
+        while(i>=0 && nums[i]>=nums[i+1]){//this is to find the pivot,means where nums[i]<nums[i+1]
             i--;
         }
 
-        if(i>=0){
+        if(i>=0){//if the pivot exists then run this code
             int j=n-1;
-            while(j>=0 && nums[i]>=nums[j]){
+            while(j>=0 && nums[i]>=nums[j]){//this is to find the next greater element means nums[i]<nums[j]
                 j--;
             }
             swap(nums,i,j);
         }
         
-        reverse(nums,i+1,n-1);
+        reverse(nums,i+1,n-1);//reverse only this portion
     }
 }
