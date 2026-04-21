c8b0d6a785ac50d5eeff7bdc6a478976e7ac3020
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-04-21 11:00:24 +0530

    Time: 1 ms (88.09%) | Memory: 47.6 MB (52.51%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,103 @@
+class Solution {
+    public void setZeroes(int[][] arr) {
+        // int m=arr.length,n=arr[0].length;
+        // boolean zeroRow=false;
+        // boolean zeroCol=false;
+        // //traverse oth column
+        // for(int i=0;i<m;i++){
+        //     if(arr[i][0]==0){
+        //         zeroCol=true;
+        //          break;
+        //     }
+
+        // }
+        // //traverse oth row
+        // for(int j=0;j<n;j++){
+        //     if(arr[0][j]==0){
+        //         zeroRow=true;
+        //          break;
+        //     }
+        // }
+        // //traverse without oth row and column
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         if(arr[i][j]==0){
+        //             arr[i][0]=0;
+        //             arr[0][j]=0;
+        //         }
+        //     }
+        // }
+        // for(int i=1;i<m;i++){//set the 0 marked rows to 0
+        //     if(arr[i][0]==0){
+        //         for(int j=1;j<n;j++){
+        //             arr[i][j]=0;
+        //         }
+        //     }
+        // }
+        // for(int j=1;j<n;j++){//set the 0 marked columns to 0
+        //     if(arr[0][j]==0){
+        //         for(int i=1;i<m;i++){
+        //             arr[i][j]=0;
+        //         }
+        //     }
+        // }
+        // if(zeroRow==true){//set the 0th row to zero
+        //     for(int j=0;j<n;j++){
+        //         arr[0][j]=0;
+        //     }
+        // }
+        // if(zeroCol==true){//set the 0th column to zero
+        //     for(int i=0;i<m;i++){
+        //         arr[i][0]=0;
+        //     }
+        // }
+
+        int m = arr.length, n = arr[0].length;
+        boolean zeroRow = false;
+        boolean zeroCol = false;
+        for (int i = 0; i < m; i++) {//check the 0th column
+            if (arr[i][0] == 0) {
+                zeroCol = true;
+                break;
+            }
+        }
+        for (int j = 0; j < n; j++) {//check the 0th row
+            if (arr[0][j] == 0) {
+                zeroRow = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {//check other than 0th row and 0th column
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == 0) {
+                    arr[i][0] = 0;
+                    arr[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {//make the marked column 0
+            if (arr[i][0] == 0) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < n; j++) {//make the marked row 0
+            if (arr[0][j] == 0) {
+                for (int i = 0; i < m; i++) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        if (zeroRow == true) {//make the 0th column 0
+            for (int j = 0; j < n; j++) {
+                    arr[0][j] = 0;
+            }
+        }
+        if (zeroCol == true) {//make the 0th row 0
+            for (int i = 0; i < m; i++) {
+                    arr[i][0] = 0;
+            }
+        }
+    }
+}
