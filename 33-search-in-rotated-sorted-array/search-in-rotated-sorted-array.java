8ec9e3f923446726d3f646b0bb9c2c01c09497fd
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-05-13 10:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (45.41%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int search(int[] arr, int target) {
-        int n=arr.length;
-        int lo=0,hi=n-1;
-        while(hi>=lo){
-            int mid=lo+(hi-lo)/2;
-            if(arr[mid]==target) return mid;
-            else if(arr[mid]<=arr[hi]){// in right sorted array
-                if(arr[mid]<target && target<=arr[hi]) lo=mid+1;
-                else hi=mid-1;
-            }
-            else{//in left sorted array
-                if(arr[lo]<=target && target<arr[mid]) hi=mid-1;
-                else lo=mid+1;
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int hi = n - 1, lo = 0;
+        while (lo<=hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] <= nums[hi]) {//left sorted array
+                if (nums[mid] < target && target <= nums[hi])
+                    lo = mid + 1;//go to right side
+                else
+                    hi = mid - 1;//go to left side
+            } else {//right sorted array
+                if (nums[mid] > target && target >= nums[lo])
+                    hi = mid - 1;//go to left side
+                else
+                    lo = mid + 1;//go to right side
             }
         }
         return -1;
