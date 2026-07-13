d72abb0d637a73ac93dae691c47c60e683a7dbb5
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-07-13 10:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (99.61%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double power(double x,long n){
+        if(n==0) return 1.0;
+        double half=power(x,n/2);
+        if(n%2==0) return half*half;
+        else return half*half*x;
+    }
+    public double myPow(double x, int n) {
+        long N=n;
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        return power(x,N);
+    }
+}
