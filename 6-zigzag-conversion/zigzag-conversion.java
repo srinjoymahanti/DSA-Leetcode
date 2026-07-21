3344db2e4e3d33efd53867941f63105c5358a450
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-07-21 18:24:34 +0530

    Time: 6 ms (29.07%) | Memory: 46.3 MB (89.22%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1) return s;
+        StringBuilder ans=new StringBuilder();
+        int increment=2*(numRows-1);
+        for(int r=0;r<numRows;r++){
+            for(int i=r;i<s.length();i+=increment){
+                ans.append(s.charAt(i));//for normal vertical printing
+                
+                //for adding diagonal elements only
+                int diagonal=i+increment-2*r;
+                if(0<r && r<numRows-1 && diagonal<s.length()){
+                    ans.append(s.charAt(diagonal));
+                }
+            }
+        }
+        return ans.toString();
+    }
+}
