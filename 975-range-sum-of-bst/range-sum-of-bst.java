701cf82bbfbfaa3bf764e07e4a1644e9d56d11b1
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-07-27 22:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (64.49%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -16,8 +16,8 @@
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root==null) return 0;
-        int sum=rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
-        if(low<=root.val && root.val<=high) sum+=root.val;
-        return sum;
+        if(root.val<low) return rangeSumBST(root.right,low,high);
+        else if(root.val>high) return rangeSumBST(root.left,low,high);
+        return root.val+rangeSumBST(root.right,low,high)+rangeSumBST(root.left,low,high);
     }
 }
