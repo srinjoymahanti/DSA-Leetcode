9c654bd7c991724301a9c95d9bbb3ce9318c02a2
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-07-31 21:46:47 +0530

    Time: 2 ms (12.69%) | Memory: 45.5 MB (10.51%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,20 +14,18 @@
  * }
  */
 class Solution {
-    public long max(TreeNode root){
-        if(root==null) return Long.MIN_VALUE;
-        long a=root.val,b=max(root.left),c=max(root.right);
-        return Math.max(a,Math.max(b,c));
-    }
-    public long min(TreeNode root){
-        if(root==null) return Long.MAX_VALUE;
-        long a=root.val,b=min(root.left),c=min(root.right);
-        return Math.min(a,Math.min(b,c));
+    public void inOrder(TreeNode root,List<Integer> list){
+        if(root==null) return;
+        inOrder(root.left,list);
+        list.add(root.val);
+        inOrder(root.right,list);
     }
     public boolean isValidBST(TreeNode root) {
-        if(root==null) return true;
-        if(root.val<=max(root.left)) return false;
-        if(root.val>=min(root.right)) return false;
-        return isValidBST(root.left) && isValidBST(root.right);
+        List<Integer> list=new ArrayList<>();
+        inOrder(root,list);
+        for(int i=1;i<list.size();i++){
+            if(list.get(i)<=list.get(i-1)) return false;
+        }
+        return true;
     }
 }
