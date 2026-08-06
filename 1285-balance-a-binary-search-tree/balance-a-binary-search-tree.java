7745a079a57b692add4c3cf4adbe01363d446c8d
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-08-06 17:02:19 +0530

    Time: 2 ms (99.19%) | Memory: 48.3 MB (25.49%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -14,22 +14,22 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode node,List<Integer> list){
+    public void inorder(TreeNode node,List<TreeNode> list){
         if(node==null) return;
         inorder(node.left,list);
-        list.add(node.val);
+        list.add(node);
         inorder(node.right,list);
     }
-    public TreeNode convertToBST(List<Integer> list,int low,int high){
+    public TreeNode convertToBST(List<TreeNode> list,int low,int high){
         if(low>high) return null;
         int mid=low+(high-low)/2;
-        TreeNode node =new TreeNode(list.get(mid));
+        TreeNode node=list.get(mid);
         node.left=convertToBST(list,low,mid-1);
         node.right=convertToBST(list,mid+1,high);
         return node;
     }
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
+        List<TreeNode> list=new ArrayList<>();
         inorder(root,list);
         return convertToBST(list,0,list.size()-1);
     }
