ee86bb7d9bbb4c19b600d86024c3b567c1844f25
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-08-15 16:32:20 +0530

    Time: 1 ms (16.25%) | Memory: 45.3 MB (35.09%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,12 +14,40 @@
  * }
  */
 class Solution {
-    public boolean helper(TreeNode root,long min,long max){
-        if(root==null) return true;
-        if(root.val<=min || max<=root.val) return false;
-        return helper(root.left,min,root.val)&& helper(root.right,root.val,max);
-    }
+    // public boolean helper(TreeNode root,long min,long max){
+    //     if(root==null) return true;
+    //     if(root.val<=min || max<=root.val) return false;
+    //     return helper(root.left,min,root.val)&& helper(root.right,root.val,max);
+    // }
+    // public boolean isValidBST(TreeNode root) {
+    //     return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    // }
+
+
     public boolean isValidBST(TreeNode root) {
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        TreeNode prev=null;
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null && pred.right!=curr) pred=pred.right;
+                if(pred.right==null){
+                    pred.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    pred.right=null;
+                    if(prev!=null && prev.val>=curr.val) return false;
+                    prev=curr;
+                    curr=curr.right;
+                }
+            }
+            else{
+                if(prev!=null && prev.val>=curr.val) return false;
+                prev=curr;
+                curr=curr.right;
+            }
+        }
+        return true;
     }
 }
