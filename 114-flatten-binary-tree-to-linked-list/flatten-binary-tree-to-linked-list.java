2a3f8964d294ee73ef928284be307439d4b5b99f
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-08-17 09:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (16.73%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -14,19 +14,34 @@
  * }
  */
 class Solution {
-    public void preorder(TreeNode root,List<TreeNode> list){
-        if(root==null) return;
-        list.add(root);
-        preorder(root.left,list);
-        preorder(root.right,list);
-    }
+    // public void preorder(TreeNode root,List<TreeNode> list){
+    //     if(root==null) return;
+    //     list.add(root);
+    //     preorder(root.left,list);
+    //     preorder(root.right,list);
+    // }
+    // public void flatten(TreeNode root) {
+    //     List<TreeNode> list=new ArrayList<>();
+    //     preorder(root,list);
+    //     int n=list.size();
+    //     for(int i=0;i<n-1;i++){
+    //         list.get(i).right=list.get(i+1);
+    //         list.get(i).left=null;
+    //     }
+    // }
+
+
     public void flatten(TreeNode root) {
-        List<TreeNode> list=new ArrayList<>();
-        preorder(root,list);
-        int n=list.size();
-        for(int i=0;i<n-1;i++){
-            list.get(i).right=list.get(i+1);
-            list.get(i).left=null;
-        }
+        if(root==null) return;
+        if(root.left==null && root.right==null) return;
+        TreeNode lst=root.left;
+        TreeNode rst=root.right;
+        flatten(lst);
+        flatten(rst);
+        root.left=null;
+        root.right=lst;
+        TreeNode temp=root;
+        while(temp.right!=null) temp=temp.right;
+        temp.right=rst;
     }
 }
