f8122ba072a874dbe14f95a0560fe94fb9e33110
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-08-19 17:17:54 +0530

    Time: 17 ms (16.67%) | Memory: 46.8 MB (9.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -10,15 +10,43 @@ class Solution {
         // }
         // return true;
 
-        int m=s.length(),n=t.length();
-        if(m!=n) return false;
-        int[] freq=new int[26];
-        for(int i=0;i<m;i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
+        // int m=s.length(),n=t.length();
+        // if(m!=n) return false;
+        // int[] freq=new int[26];
+        // for(int i=0;i<m;i++){
+        //     freq[s.charAt(i)-'a']++;
+        //     freq[t.charAt(i)-'a']--;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]!=0) return false;
+        // }
+        // return true;
+
+
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> sMap=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char key=s.charAt(i);
+            if(sMap.containsKey(key)){
+                int freq=sMap.get(key);
+                sMap.put(key,freq+1);
+            }
+            else sMap.put(key,1);
         }
-        for(int i=0;i<26;i++){
-            if(freq[i]!=0) return false;
+        HashMap<Character,Integer> tMap=new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            char key=t.charAt(i);
+            if(tMap.containsKey(key)){
+                int freq=tMap.get(key);
+                tMap.put(key,freq+1);
+            }
+            else tMap.put(key,1);
+        }
+        for(char key:sMap.keySet()){
+            if(!tMap.containsKey(key)) return false;
+            int sVal=sMap.get(key);
+            int tVal=tMap.get(key);
+            if(sVal!=tVal) return false;
         }
         return true;
     }
