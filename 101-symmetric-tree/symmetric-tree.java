cd8f9447659eb7e4dc8ef70c46b75ed9dacf9eee
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-09-06 18:50:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (52.39%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -14,24 +14,24 @@
  * }
  */
 class Solution {
-    public void invert(TreeNode temp){
-        if(temp==null) return;
-        TreeNode helper=temp.left;
-        temp.left=temp.right;
-        temp.right=helper;
-        invert(temp.left);
-        invert(temp.right);
+    public void invert(TreeNode node){
+        if(node==null) return;
+        TreeNode helper=node.left;
+        node.left=node.right;
+        node.right=helper;
+        invert(node.left);
+        invert(node.right);
     }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSameTree(TreeNode p,TreeNode q){
         if(p==null && q==null) return true;
         if(p==null || q==null) return false;
-        if(p.val != q.val) return false;
+        if(p.val!=q.val) return false;
         if(!isSameTree(p.left,q.left)) return false;
         if(!isSameTree(p.right,q.right)) return false;
         return true;
     }
     public boolean isSymmetric(TreeNode root) {
         invert(root.right);
-        return isSameTree(root.left,root.right);
+        return isSameTree(root.right,root.left);
     }
 }
