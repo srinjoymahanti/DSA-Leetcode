e750ee30695e663025c48350a81f68f1c4134a8d
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-09-20 17:02:10 +0530

    Time: 2 ms (94.23%) | Memory: 45.7 MB (67.40%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,20 @@
 class Solution {
-    public void combSum(int[] arr,int idx,List<Integer> combi,List<List<Integer>> ans,int target){
+    public void combiSum(int[] arr,List<Integer> list,List<List<Integer>> ans,int idx,int target){
         if(idx==arr.length || target<0) return;
-        if(target==0) {
-            ans.add(new ArrayList<>(combi));
+        if(target==0){
+            ans.add(new ArrayList(list));
             return;
         }
-        combi.add(arr[idx]);
-        // combSum(arr,idx+1,combi,ans,target-arr[idx]);//include single time
-        combSum(arr,idx,combi,ans,target-arr[idx]);//include multiple times
-        combi.remove(combi.size()-1);//excluded the element
-        combSum(arr,idx+1,combi,ans,target);//exclude the element
+        list.add(arr[idx]);
+        combiSum(arr,list,ans,idx,target-arr[idx]);
+
+        list.remove(list.size()-1);
+        combiSum(arr,list,ans,idx+1,target);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> combi=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         List<List<Integer>> ans=new ArrayList<>();
-        combSum(candidates,0,combi,ans,target);
+        combiSum(candidates,list,ans,0,target);
         return ans;
     }
 }
