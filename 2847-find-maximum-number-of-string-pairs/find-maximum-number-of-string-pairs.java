db1a24837c45b3b6cbf0cfd203e72530eedbfd68
Srinjoy Mahanti <148843577+srinjoymahanti@users.noreply.github.com>
2026-09-21 16:21:58 +0530

    Time: 3 ms (82.55%) | Memory: 45.1 MB (57.01%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
@@ -1,21 +1,16 @@
 class Solution {
+    private String reverse(String str){
+        StringBuilder sb=new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
     public int maximumNumberOfStringPairs(String[] words) {
         HashSet<String> set=new HashSet<>();
         int count=0;
         for(int i=0;i<words.length;i++){
-            if(set.contains(words[i])) continue;
-            else{
-                set.add(words[i]);
-                for(int j=i+1;j<words.length;j++){
-                    char char1=words[i].charAt(0);
-                    char char2=words[i].charAt(1);
-                    String word=""+char2+char1;
-                    if(word.equals(words[j])){
-                        count++;
-                        set.add(words[j]);
-                    }
-                }
-            }
+            String rev=reverse(words[i]);
+            if(set.contains(rev)) count++;
+            else set.add(words[i]);
         }
         return count;
     }
